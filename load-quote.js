00a08d8a50c46651176a4c8fbde595a0948e5014
David Ernst <git@dsernst.com>
2015-10-04 13:24:11 -0700

    Load yaml file asynchronously

--- load-quote.js
+++ load-quote.js
@@ -2,11 +2,12 @@
 
 var yaml = require('yamljs')
 
-var quotes = yaml.load('data_favorite_quotes.yml')
-var quote = quotes[Math.floor(Math.random() * quotes.length)]
+yaml.load('data_favorite_quotes.yml', function (quotes) {
+  var quote = quotes[Math.floor(Math.random() * quotes.length)]
 
-var blockquote = document.querySelectorAll('blockquote')[0]
-var cite = document.querySelectorAll('cite')[0]
+  var blockquote = document.querySelectorAll('blockquote')[0]
+  var cite = document.querySelectorAll('cite')[0]
 
-blockquote.textContent = quote.quote
-cite.textContent = quote.author || 'Unknown'
+  blockquote.textContent = quote.quote
+  cite.textContent = quote.author || 'Unknown'
+})
