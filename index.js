15ff448446719d0431521b92f447995a40ae5f97
David Ernst <git@dsernst.com>
2015-10-03 23:57:07 -0700

    Console.log random quote from collection

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var yaml = require('js-yaml')
 var fs = require('fs')
+var _ = require('lodash')
 
 try {
   var doc = yaml.safeLoad(fs.readFileSync('./data_favorite_quotes.yml', 'utf8'));
-  console.log(doc);
+  console.log(_.sample(doc));
+
 } catch (e) {
   console.log(e);
 }
