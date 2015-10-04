1b639a176a368c8e29cff3525163e66dc699a9ef
David Ernst <git@dsernst.com>
2015-10-04 13:15:43 -0700

    Display a random quote each time the page is reloaded

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var _ = require('lodash')
-var YAML = require('yamljs')
+/* eslint-env browser */
 
-try {
-  var doc = YAML.load('data_favorite_quotes.yml')
-  console.log(_.sample(doc));
-} catch (e) {
-  console.log(e);
-}
+var yaml = require('yamljs')
+
+var quotes = yaml.load('data_favorite_quotes.yml')
+var quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+var blockquote = document.querySelectorAll('blockquote')[0]
+var cite = document.querySelectorAll('cite')[0]
+
+blockquote.textContent = quote.quote
+cite.textContent = quote.author || 'Unknown'
