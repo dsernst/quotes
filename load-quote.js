6938a852ee964d279ab8a5f3fffbf3e2f881ae19
David Ernst <git@dsernst.com>
2015-10-04 14:35:32 -0700

    Rename data_favorite_quotes.yml to quotes.yml

--- load-quote.js
+++ load-quote.js
@@ -4,7 +4,7 @@
 
 var yaml = require('yamljs')
 
-yaml.load('data_favorite_quotes.yml', function (quotes) {
+yaml.load('quotes.yml', function (quotes) {
   var quote = quotes[Math.floor(Math.random() * quotes.length)]
 
   var blockquote = document.querySelectorAll('blockquote')[0]
